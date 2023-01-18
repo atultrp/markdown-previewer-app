a8595efdb88332d34ad600efaec16e03c68fe5b4
Atul Tripathi <atul31815@gmail.com>
2023-01-18 22:55:56 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <main>
-        <h1 className='text-green-400'>Markdown preview</h1>
+        <h1 className='my-5 text-3xl uppercase font-semibold font-mono text-center'>Markdown Previewer</h1>
+        <div className='grid grid-cols-2'>
+          <div className='mx-4 bg-red-300'>
+            <h2 className='text-2xl'>Editor</h2>
+            <textarea id="editor" placeholder="Enter your markdown here" className='w-full'></textarea>
+          </div>
+          <div className='mx-4 bg-red-300'>
+            <div id="preview">Preview</div>
+          </div>
+        </div>
       </main>
     </>
   )
